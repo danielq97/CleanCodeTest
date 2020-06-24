f4cbc0e8de0998e8bc8dd2e41a2946c8c56906fb
danielq97 <discover_daniel@hotmail.com>
2020-06-23 23:51:37 -0500

    Roulette class with attributtes, constructor and getter and setters

--- src/main/java/com/masivian/model/Roulette.java
+++ src/main/java/com/masivian/model/Roulette.java
@@ -4,9 +4,33 @@ import java.io.Serializable;
 
 public class Roulette implements Serializable {
 
+	private int id;
+	private String status;
 
-	
-	
-	
-	
+	public Roulette() {
+
+	}
+
+	public Roulette(int id, String status) {
+		super();
+		this.id = id;
+		this.status = status;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 }
